29a74c16c2437b227ac7cd42a890026ef07483d5
jiho <jxxo.ih@gmail.com>
2023-09-18 10:13:15 +0900

    0918

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -3,15 +3,20 @@
 import { Plus } from "lucide-react";
 
 import { ActionTooltip } from "@/components/action-tooltip";
+import { useModal } from "@/hooks/use-modal-store";
 
 export const NavigationAction = () => {
+    const { onOpen } = useModal();
+
     return (
         <div>
             <ActionTooltip
-            side="right"
-            align="center"
-            label="Add a server">
-                <button className="group flex items-center">
+                side="right"
+                align="center"
+                label="Add a server">
+                <button
+                    onClick={() => onOpen("createServer")}
+                    className="group flex items-center">
                     <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] 
                 group-hover:rounded-[16px] transition-all overflow-hidden 
                 items-center justify-center bg-background 
